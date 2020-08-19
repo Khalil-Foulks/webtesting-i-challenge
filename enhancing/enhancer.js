376ffd4d9ab5b1faf216d8924c695bea1017933c
Khalil <khalilfoulks@gmail.com>
2020-08-19 18:29:10 -0400

    finished fail testing

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -9,7 +9,7 @@ function success(item) {
   const enhancedItem = {...item}
 
   if(enhancedItem.enhancement === 20){
-    return enhancedItem
+    return enhancedItem;
   }else {
     enhancedItem.enhancement += 1 
     return enhancedItem; 
@@ -17,7 +17,26 @@ function success(item) {
 }
 
 function fail(item) {
-  return { ...item };
+  const failedItem = {...item}
+
+  if(failedItem.enhancement >= 15){
+    if(failedItem.enhancement > 16){
+      failedItem.durability -= 10
+      failedItem.enhancement -= 1
+
+    }else {
+      failedItem.durability -= 10
+    }
+
+  }else if(failedItem.enhancement < 15){
+    failedItem.durability -= 5   
+  }
+
+  if(failedItem.durability < 0){
+    failedItem.durability = 0
+  }
+  
+  return failedItem
 }
 
 function repair(item) {
